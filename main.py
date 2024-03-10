4ee0e6b3f1b96a42e849e4af8f0cefe6840d4277
ivanhoe <ayvengo85@gmail.com>
2024-03-10 21:59:15 +0200

    10.03.24

--- main.py
+++ main.py
@@ -1,22 +1,61 @@
+# from flask import Flask, request
+# from flask.views import MethodView
+#
+#
+# app = Flask(__name__)
+#
+# @app.route('/', methods=["POST", "GET"])
+# def index():
+#     if request.method == "POST":
+#         resp = request.get_json()
+#         print(resp)
+#         return '<h1>Hi Telegram</h1>'
+#     return '<h1>Hi Bot</h1>'
+#
+#
+# class BotAPI(MethodView):
+#     def get(self):
+#         return '<h1>Hi Bot_Class</h1>'
+#
+#     def post(self):
+#         resp = request.get_json()
+#         print(resp)
+#         return '<h1>Hi Telegram_Class</h1>'
+#
+#
+# app.add_url_rule('/TOKEN/', view_func=BotAPI.as_view('bot'))
+#
+# if __name__ == "__main__":
+#     app.run()
+
+
 import requests
 import os
 from dotenv import load_dotenv
 from flask import Flask, request
 from flask.views import MethodView
+
 load_dotenv()
 
 app = Flask(__name__)
 TOKEN = os.environ.get('TOKEN')
 TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
-def send_message(chat_id, tmp):
-    pass
+
+def send_message(chat_id, msg):
+    session = requests.Session()
+    r = session.get(TELEGRAM_URL, params=dict(chat_id=chat_id,
+                                              text=msg,
+                                              parse_mode='Markdown'))
+    return r.json()
+
+
 def parse_text(text_msg):
     if '/' in text_msg:
         if '/start' in text_msg or '/help' in text_msg:
-            message = '''Для того, чтобы узнать, какие города доступны, отправьте в сообщении `/city`. 
-            Чтобы узнать о доступных специальностях - отправьте `/sp` 
-            Чтобы сделать запрос на сохраненные вакансии, отправьте в сообщении через пробел - @город @специальность. 
+            message = '''Для того, чтобы узнать, какие города доступны, отправьте в сообщении `/city`.
+            Чтобы узнать о доступных специальностях - отправьте `/sp`
+            Чтобы сделать запрос на сохраненные вакансии, отправьте в сообщении через пробел - @город @специальность.
             Например так - `@kyiv @python`  '''
             return message
     else:
@@ -34,7 +73,6 @@ def index():
 
 class BotAPI(MethodView):
 
-
     def get(self):
         return '<h1>Hi Bot_Class</h1>'
 
@@ -48,10 +86,12 @@ class BotAPI(MethodView):
         print(resp)
         return '<h1>Hi Telegram_Class</h1>'
 
-app.add_url_rule('/TOKEN/',view_func=BotAPI.as_view('bot'))
+
+app.add_url_rule('/TOKEN/', view_func=BotAPI.as_view('bot'))
 
 if __name__ == "__main__":
     app.run()
 
 
-
+.env.py
+TOKEN=7071566085:AAFA6NfFl8rVNC8g5RPgrBco8OnP-pg1tDc
